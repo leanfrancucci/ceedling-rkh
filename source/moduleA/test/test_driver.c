4f51cd34b6b81d4bd6af8470b4797e566d3e9e0c
lf <francuccilea@gmail.com>
2018-08-24 14:32:47 -0300

    Directory structure for CI with ceedling

---
+++ source/moduleA/test/test_driver.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "driver.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_driver_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement driver");
+}
